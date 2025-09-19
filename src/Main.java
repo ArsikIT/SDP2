87866c436c84d14776420ef1af8735b76528767e
ArsikIT <arslan.zheksenbayev@mail.ru>
2025-09-19 12:34:29 +0500

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import Factory.CleanCode2.EmailFactory;
+import Factory.CleanCode2.PushFactory;
+import Factory.CleanCode2.SmsFactory;
+import Factory.NotificationFactory;
 import NotificationProduct.CleanCode.EmailNotification;
 import NotificationProduct.Notification;
 
 public class Main {
     public static void main(String[] args) {
-        Notification notification = new EmailNotification();
+        NotificationFactory factory = new SmsFactory();
+        Notification notification = factory.createNotification();
+
         notification.sendNotification();
 
     }
